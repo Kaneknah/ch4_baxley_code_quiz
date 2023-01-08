94c26467d424f6031ccffba9e8c07b754a54ef18
Kaneknah <jamesnbaxley@protonmail.com>
2023-01-07 17:30:06 -0700

    final corrections

--- develop/script.js
+++ develop/script.js
@@ -16,139 +16,113 @@ const clearBtn = $("#clear-btn");
 let timeRemainingEl = $("#time-remaining");
 const quizPrompt = $("#quiz-prompt");
 const startBtn = $("#start-btn");
-let questionCard = (".question-card");
+let questionCard = ".question-card";
 let answerOptions = $(".answer-options");
 let questionPrompt = $("#question-prompt");
-let highScoreEl = ("");
+let highScoreEl = "";
 let currentQuestionInt = 0;
 let secondsLeft = 30;
 let resultsText = $(".results-text");
+let score = $("#score");
 // let chosenQuestion = ""
 
 //create the questions needed for the quiz
-var questions = [ 
-    {
-        question: "Is Andrew a Dingus?",
-        options: [
-        "A. Yes",
-        "B. Duh",
-        "C. Fuck yeah",],
-        answer: "Fuck yeah",
-         } ,
+var questions = [
+	{
+		question: "Is Andrew a Dingus?",
+		options: ["A. Yes", "B. Duh", "C. Fuck yeah"],
+		answer: "C. Fuck yeah",
+	},
 
-      {
-        question: "Bla Bla Bla222",
-        options: [
-        "A. bla",
-        "B. bla",
-        "C. Bla",],
-        answer: 2,
-         } ,
-    
-     {
-        question: "Bla Bla Bla333",
-        options: [
-        "A. bla",
-        "B. bla",
-        "C. Bla",],
-        answer: 1,
-         } ,
-    
-  {
-        question: "Bla Bla Bla444",
-        options: [
-        "A. bla",
-        "B. bla",
-        "C. Bla",],
-        answer: 0,
-         } ,
-    
-  {
-        question: "Bla Bla Bla555",
-        options: [
-        "A. bla",
-        "B. bla",
-        "C. Bla",],
-        answer: 2,
-         } ,
-    
+	{
+		question: "Bla Bla Bla222",
+		options: ["A. bla", "B. bla", "C. Bla"],
+		answer: 2,
+	},
+
+	{
+		question: "Bla Bla Bla333",
+		options: ["A. bla", "B. bla", "C. Bla"],
+		answer: 1,
+	},
+
+	{
+		question: "Bla Bla Bla444",
+		options: ["A. bla", "B. bla", "C. Bla"],
+		answer: 0,
+	},
+
+	{
+		question: "Bla Bla Bla555",
+		options: ["A. bla", "B. bla", "C. Bla"],
+		answer: 2,
+	},
 ];
 //Function for moving to the next question.
-function getNextQuestion(questionNum){
-  currentQuestionInt++
-  return (questions[questionNum]);
+function getNextQuestion(questionNum) {
+	currentQuestionInt++;
+	return questions[questionNum];
 }
 
-// Function for Starting the quiz: removes buttons and starts timer and activates runQuiz function. 
-function startQuiz(){
+// Function for Starting the quiz: removes buttons and starts timer and activates runQuiz function.
+function startQuiz() {
+	var timeInterval = setInterval(function () {
+		secondsLeft--;
+		timeRemainingEl.text(`Time Remaining: ${secondsLeft}`);
+		if (secondsLeft === 0) {
+			clearInterval(timeInterval);
+		}
+	}, 1000);
 
-  var timeInterval = setInterval(function() {
-    secondsLeft--;
-    timeRemainingEl.text(`Time Remaining: ${secondsLeft}`)
-    if(secondsLeft === 0) {
-      clearInterval(timeInterval);
-    
-    }
+	startBtn.remove();
+	quizPrompt.remove();
 
-  }, 1000);
-
-  startBtn.remove()
-  quizPrompt.remove()
-
-  let currentQuestion = getNextQuestion(currentQuestionInt);
-  displayQuestion(currentQuestion)
+	let currentQuestion = getNextQuestion(currentQuestionInt);
+	displayQuestion(currentQuestion);
 }
 
 // how to connect text in JS to specific questions and rotate them though as they are clicked.
 function displayQuestion(question) {
-  console.log(question);
-  questionPrompt.text(`Question: ${question.question}`);
-  console.log(question.options);
-  console.log(question.answer);
-  question.options.forEach(option => {
-  const answerBtn = $("<button></button>")
-  answerOptions.append(answerBtn.text(option));
-  answerBtn.on('click', answerResults)
-  
-  })
-  
-//  answerBtn.on("click", answerResults());
+	questionPrompt.text(`Question: ${question.question}`);
+	question.options.forEach((option) => {
+		const answerBtn = $("<button></button>").on("click", answerResults);
+		answerOptions.append(answerBtn.text(option));
+	});
 
-
+	//  answerBtn.on("click", answerResults());
 }
 
 //Function for when the user selects an answer
-function answerResults(event){
-  let selectedAnswer = $(event.target)
-  console.log(selectedAnswer)
-  if (selectedAnswer = questions.answer){
-    correctAnswer();
-    
-  }else {
-    wrongAnswer();
-     }
-  
-  getNextQuestion(question);
-    }
+function answerResults(event) {
+	let selectedAnswer = $(event.target).val;
+	console.log(selectedAnswer);
+	if ((question = questions.answer)) {
+		correctAnswer();
+	} else {
+		wrongAnswer();
+	}
+
+	getNextQuestion(questions);
+}
 //Function for if the wrong answer was clicked.
-    function wrongAnswer() {
-      console.log("Idiot");
-      secondsLeft - 5;
-      let responseText = "Sorry Wrong Answer"
-    resultsText.append(responseText);
-    }
+function wrongAnswer() {
+	console.log("Idiot");
+	secondsLeft - 5;
+	let responseText = "Sorry Wrong Answer";
+	resultsText.append(responseText);
+}
 //function for if the right answer was clicked.
-    function correctAnswer(){
-      console.log("good job");
-      let responseText = "Congrats! you are right!"
-    resultsText.append(responseText);
-    }
+function correctAnswer() {
+	console.log("good job");
+	let responseText = "Congrats! you are right!";
+	score = score + 5;
+	resultsText.append(responseText);
+}
 
-
-
+function renderScore() {}
 
 // add event listeners
- startBtn.on("click", startQuiz);
+startBtn.on("click", startQuiz);
 // homeBtn.addEventListener("click", home);
 // clearBtn.addEventListener("clikc", clear);
 // submitBtn.addEventListener("click", submit);

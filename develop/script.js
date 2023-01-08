41d72c02bf70977f420ec80af8aea3e6316de32a
Kaneknah <jamesnbaxley@protonmail.com>
2023-01-07 19:35:49 -0700

    button issue fix

--- develop/script.js
+++ develop/script.js
@@ -24,7 +24,6 @@ let currentQuestionInt = 0;
 let secondsLeft = 30;
 let resultsText = $(".results-text");
 let score = $("#score");
-// let chosenQuestion = ""
 
 //create the questions needed for the quiz
 var questions = [
@@ -85,7 +84,10 @@ function startQuiz() {
 function displayQuestion(question) {
 	questionPrompt.text(`Question: ${question.question}`);
 	question.options.forEach((option) => {
-		const answerBtn = $("<button></button>").on("click", answerResults);
+		const answerBtn = $("<button></button>").click(
+			{ question: question },
+			answerResults
+		);
 		answerOptions.append(answerBtn.text(option));
 	});
 
@@ -94,20 +96,21 @@ function displayQuestion(question) {
 
 //Function for when the user selects an answer
 function answerResults(event) {
-	let selectedAnswer = $(event.target).val;
-	console.log(selectedAnswer);
-	if ((question = questions.answer)) {
+	let selectedAnswer = $(event.target).text();
+	if (selectedAnswer === event.data.question.answer) {
 		correctAnswer();
 	} else {
 		wrongAnswer();
 	}
-
-	getNextQuestion(questions);
+	let currentQuestion = getNextQuestion(currentQuestionInt);
+	// getNextQuestion(questions);
+	displayQuestion(currentQuestion);
+	//add delete current button function and add a call here.
 }
 //Function for if the wrong answer was clicked.
 function wrongAnswer() {
 	console.log("Idiot");
-	secondsLeft - 5;
+	secondsLeft -= 5;
 	let responseText = "Sorry Wrong Answer";
 	resultsText.append(responseText);
 }
@@ -115,7 +118,7 @@ function wrongAnswer() {
 function correctAnswer() {
 	console.log("good job");
 	let responseText = "Congrats! you are right!";
-	score = score + 5;
+	score += 5;
 	resultsText.append(responseText);
 }
 
@@ -124,5 +127,5 @@ function renderScore() {}
 // add event listeners
 startBtn.on("click", startQuiz);
 // homeBtn.addEventListener("click", home);
-// clearBtn.addEventListener("clikc", clear);
+// clearBtn.addEventListener("click", clear);
 // submitBtn.addEventListener("click", submit);
